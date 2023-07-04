5039308c5b68342c922c3d23b126af2e39e38a2c
being <lizhuofeng12345@gmail.com>
2023-07-04 10:06:55 +0800

    compress get_time_series

--- graph_lstm_vae_ad_ver6/graphlstm.py
+++ graph_lstm_vae_ad_ver6/graphlstm.py
@@ -10,7 +10,7 @@ from .algorithm_utils import Algorithm, PyTorchUtils
 
 class GCNLSTMCell(nn.Module, PyTorchUtils):
 
-    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int=0, gpu: int=None):
+    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int = 0, gpu: int = None):
         """
         Initialize GCNLSTM cell.
         
@@ -30,15 +30,15 @@ class GCNLSTMCell(nn.Module, PyTorchUtils):
         PyTorchUtils.__init__(self, seed, gpu)
 
         self.nodes_num = nodes_num
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
         self.bias = bias
-        
+
         self.gconv = GCNConv(in_channels=self.input_dim + self.hidden_dim,
                              out_channels=4 * self.hidden_dim,
                              bias=self.bias,
-                             improved = True)
+                             improved=True)
 
     def forward(self, input_tensor, cur_state, edge_index):
         '''
@@ -46,14 +46,14 @@ class GCNLSTMCell(nn.Module, PyTorchUtils):
         cur_state:[(b,n,h),(b,n,h)]
         '''
         h_cur, c_cur = cur_state
-        
+
         combined = torch.cat([input_tensor, h_cur], dim=2)  # concatenate along hidden axis
         batch = Batch.from_data_list([Data(x=combined[i], edge_index=edge_index) for i in range(combined.shape[0])])
-        
-        combined_conv = self.gconv(batch.x, batch.edge_index)
-        combined_conv = combined_conv.reshape(combined.shape[0],combined.shape[1],-1)
 
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2) 
+        combined_conv = self.gconv(batch.x, batch.edge_index)
+        combined_conv = combined_conv.reshape(combined.shape[0], combined.shape[1], -1)
+
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2)
         i = torch.sigmoid(cc_i)
         f = torch.sigmoid(cc_f)
         o = torch.sigmoid(cc_o)
@@ -69,9 +69,10 @@ class GCNLSTMCell(nn.Module, PyTorchUtils):
         return (self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))),
                 self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))))
 
+
 class GATLSTMCell(nn.Module, PyTorchUtils):
 
-    def __init__(self, nodes_num, input_dim, hidden_dim, head=1, dropout=0, bias=True, seed: int=0, gpu: int=None):
+    def __init__(self, nodes_num, input_dim, hidden_dim, head=1, dropout=0, bias=True, seed: int = 0, gpu: int = None):
         """
         Initialize GCNLSTM cell.
         
@@ -95,16 +96,16 @@ class GATLSTMCell(nn.Module, PyTorchUtils):
         PyTorchUtils.__init__(self, seed, gpu)
 
         self.nodes_num = nodes_num
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.head = head
         self.dropout = dropout
         self.bias = bias
-        
+
         self.gconv = GATConv(in_channels=self.input_dim + self.hidden_dim,
                              out_channels=4 * self.hidden_dim,
                              heads=self.head,
-                             concat = False,
+                             concat=False,
                              dropout=self.dropout,
                              bias=self.bias)
 
@@ -117,11 +118,11 @@ class GATLSTMCell(nn.Module, PyTorchUtils):
 
         combined = torch.cat([input_tensor, h_cur], dim=2)  # concatenate along hidden axis
         batch = Batch.from_data_list([Data(x=combined[i], edge_index=edge_index) for i in range(combined.shape[0])])
-        
-        combined_conv = self.gconv(batch.x, batch.edge_index)
-        combined_conv = combined_conv.reshape(combined.shape[0],combined.shape[1],-1)
 
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2) 
+        combined_conv = self.gconv(batch.x, batch.edge_index)
+        combined_conv = combined_conv.reshape(combined.shape[0], combined.shape[1], -1)
+
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2)
         i = torch.sigmoid(cc_i)
         f = torch.sigmoid(cc_f)
         o = torch.sigmoid(cc_o)
@@ -129,7 +130,7 @@ class GATLSTMCell(nn.Module, PyTorchUtils):
 
         c_next = f * c_cur + i * g
         h_next = o * torch.tanh(c_next)
-        
+
         # output: (b,n,h),(b,n,h)
         return h_next, c_next
 
@@ -137,9 +138,10 @@ class GATLSTMCell(nn.Module, PyTorchUtils):
         return (self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))),
                 self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))))
 
+
 class WL1LSTMCell(nn.Module, PyTorchUtils):
 
-    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int=0, gpu: int=None):
+    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int = 0, gpu: int = None):
         """
         Initialize GCNLSTM cell.
         
@@ -159,14 +161,14 @@ class WL1LSTMCell(nn.Module, PyTorchUtils):
         PyTorchUtils.__init__(self, seed, gpu)
 
         self.nodes_num = nodes_num
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
         self.bias = bias
-        
+
         self.gconv = GraphConv(in_channels=self.input_dim + self.hidden_dim,
                                out_channels=4 * self.hidden_dim,
-                               aggr = 'mean',
+                               aggr='mean',
                                bias=self.bias)
 
     def forward(self, input_tensor, cur_state, edge_index):
@@ -175,14 +177,14 @@ class WL1LSTMCell(nn.Module, PyTorchUtils):
         cur_state:[(b,n,h),(b,n,h)]
         '''
         h_cur, c_cur = cur_state
-        
+
         combined = torch.cat([input_tensor, h_cur], dim=2)  # concatenate along hidden axis
         batch = Batch.from_data_list([Data(x=combined[i], edge_index=edge_index) for i in range(combined.shape[0])])
-        
-        combined_conv = self.gconv(batch.x, batch.edge_index)
-        combined_conv = combined_conv.reshape(combined.shape[0],combined.shape[1],-1)
 
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2) 
+        combined_conv = self.gconv(batch.x, batch.edge_index)
+        combined_conv = combined_conv.reshape(combined.shape[0], combined.shape[1], -1)
+
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2)
         i = torch.sigmoid(cc_i)
         f = torch.sigmoid(cc_f)
         o = torch.sigmoid(cc_o)
@@ -198,9 +200,10 @@ class WL1LSTMCell(nn.Module, PyTorchUtils):
         return (self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))),
                 self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))))
 
+
 class LSTMCell(nn.Module, PyTorchUtils):
 
-    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int=0, gpu: int=None):
+    def __init__(self, nodes_num, input_dim, hidden_dim, bias=True, seed: int = 0, gpu: int = None):
         """
         Initialize GCNLSTM cell.
         
@@ -220,11 +223,11 @@ class LSTMCell(nn.Module, PyTorchUtils):
         PyTorchUtils.__init__(self, seed, gpu)
 
         self.nodes_num = nodes_num
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
         self.bias = bias
-        
+
         self.gconv = nn.Linear(self.input_dim + self.hidden_dim,
                                4 * self.hidden_dim,
                                bias=self.bias)
@@ -235,14 +238,14 @@ class LSTMCell(nn.Module, PyTorchUtils):
         cur_state:[(b,n,h),(b,n,h)]
         '''
         h_cur, c_cur = cur_state
-        
-        combined = torch.cat([input_tensor, h_cur], dim=2)  # concatenate along hidden axis
-        #batch = Batch.from_data_list([Data(x=combined[i], edge_index=edge_index) for i in range(combined.shape[0])])
-        
-        combined_conv = self.gconv(combined)
-        #combined_conv = combined_conv.reshape(combined.shape[0],combined.shape[1],-1)
 
-        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2) 
+        combined = torch.cat([input_tensor, h_cur], dim=2)  # concatenate along hidden axis
+        # batch = Batch.from_data_list([Data(x=combined[i], edge_index=edge_index) for i in range(combined.shape[0])])
+
+        combined_conv = self.gconv(combined)
+        # combined_conv = combined_conv.reshape(combined.shape[0],combined.shape[1],-1)
+
+        cc_i, cc_f, cc_o, cc_g = torch.split(combined_conv, self.hidden_dim, dim=2)
         i = torch.sigmoid(cc_i)
         f = torch.sigmoid(cc_f)
         o = torch.sigmoid(cc_o)
@@ -258,21 +261,22 @@ class LSTMCell(nn.Module, PyTorchUtils):
         return (self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))),
                 self.to_var(Variable(torch.zeros(batch_size, self.nodes_num, self.hidden_dim))))
 
+
 class GraphLSTM(nn.Module, PyTorchUtils):
 
     def __init__(self, nodes_num, input_dim, hidden_dim, num_layers, head=1, dropout=0, kind='GCN',
-                 batch_first=False, bias=True, return_all_layers=True, seed: int=0, gpu: int=None):
+                 batch_first=False, bias=True, return_all_layers=True, seed: int = 0, gpu: int = None):
         super(GraphLSTM, self).__init__()
         PyTorchUtils.__init__(self, seed, gpu)
 
         # Make sure that `hidden_dim` are lists having len == num_layers
-        hidden_dim  = self._extend_for_multilayer(hidden_dim, num_layers)
+        hidden_dim = self._extend_for_multilayer(hidden_dim, num_layers)
         head = self._extend_for_multilayer(head, num_layers)
         if not len(hidden_dim) == len(head) == num_layers:
             raise ValueError('Inconsistent list length.')
 
         self.nodes_num = nodes_num
-        self.input_dim  = input_dim
+        self.input_dim = input_dim
         self.hidden_dim = hidden_dim
         self.num_layers = num_layers
         self.head = head
@@ -284,7 +288,7 @@ class GraphLSTM(nn.Module, PyTorchUtils):
 
         cell_list = []
         for i in range(0, self.num_layers):
-            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i-1]
+            cur_input_dim = self.input_dim if i == 0 else self.hidden_dim[i - 1]
 
             if self.kind == 'GCN':
                 cell_list.append(GCNLSTMCell(nodes_num=nodes_num,
@@ -334,11 +338,11 @@ class GraphLSTM(nn.Module, PyTorchUtils):
         last_output_list: [(b, t, n, h)] * num_layers(also determined by return_all_layers),
         layer_state_list: [[(b, n, h), (b, n, h)]] * num_layers(also determined by return_all_layers)
         """
-        #确保(t, b, n, c)
-        #if self.batch_first:
-        #写在前面了
-            # (b, t, n, c) -> (t, b, n, c)
-            #input_tensor = input_tensor.permute(1, 0, 2, 3).contiguous()
+        # 确保(t, b, n, c)
+        # if self.batch_first:
+        # 写在前面了
+        # (b, t, n, c) -> (t, b, n, c)
+        # input_tensor = input_tensor.permute(1, 0, 2, 3).contiguous()
 
         # Implement stateful GraphLSTM
         if hidden_state is not None:
@@ -348,7 +352,7 @@ class GraphLSTM(nn.Module, PyTorchUtils):
             hidden_state = self._init_hidden(input_tensor.size(1))
 
         layer_output_list = []
-        last_state_list   = []
+        last_state_list = []
 
         seq_len = input_tensor.size(0)
         cur_layer_input = input_tensor
@@ -358,9 +362,8 @@ class GraphLSTM(nn.Module, PyTorchUtils):
             h, c = hidden_state[layer_idx]
             output_inner = []
             for t in range(seq_len):
-
                 h, c = self.cell_list[layer_idx](input_tensor=cur_layer_input[t],
-                                                 edge_index = edge_index, cur_state=[h, c])
+                                                 edge_index=edge_index, cur_state=[h, c])
                 output_inner.append(h)
 
             layer_output = torch.stack(output_inner, dim=0)
@@ -371,7 +374,7 @@ class GraphLSTM(nn.Module, PyTorchUtils):
 
         if not self.return_all_layers:
             layer_output_list = layer_output_list[-1:]
-            last_state_list   = last_state_list[-1:]
+            last_state_list = last_state_list[-1:]
 
         return layer_output_list, last_state_list
 
